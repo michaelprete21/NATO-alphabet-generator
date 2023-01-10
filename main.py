c29156e0233360c26ea68b35efb36426aee939d5
michaelprete21 <106406676+michaelprete21@users.noreply.github.com>
2023-01-10 15:17:26 -0700

    updated with error exceptions

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ npa = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 npa_dict = {row.letter:row.code for (index, row) in npa.iterrows()}
 
-word = input("Please type in a word:").upper()
-output_lst = []
-try:
-    output_lst = [npa_dict[letter] for letter in word]
-except KeyError:
-    print("Please type in a word without any numbers or special characters. ")
-else:
-    print(output_lst)
+
+def generate_phonetic():
+    word = input("Please type in a word:").upper()
+    output_lst = []
+    try:
+        output_lst = [npa_dict[letter] for letter in word]
+    except KeyError:
+        print("Please type in a word without any numbers or special characters. ")
+        generate_phonetic()
+    else:
+        print(output_lst)
+
+
+generate_phonetic()
 
 
