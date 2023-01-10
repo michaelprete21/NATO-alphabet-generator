7fb6716fa8d1464e26dca790b4f73184438a7a23
michaelprete21 <106406676+michaelprete21@users.noreply.github.com>
2023-01-10 15:16:07 -0700

    updated with error exceptions

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ npa_dict = {row.letter:row.code for (index, row) in npa.iterrows()}
 
 word = input("Please type in a word:").upper()
 output_lst = []
-output_lst = [npa_dict[letter] for letter in word]
+try:
+    output_lst = [npa_dict[letter] for letter in word]
+except KeyError:
+    print("Please type in a word without any numbers or special characters. ")
+else:
+    print(output_lst)
 
-print(output_lst)
+
